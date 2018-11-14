82a359ac0a0f56a5fef2a183a4f73d742ff3ae06
kiburei <gooddeeds17@gmil.com>
2018-11-14 15:30:07 +0300

    index funct

---
+++ app/controllers/biller_controller.rb
@@ -0,0 +1,7 @@
+class BillerController < ApplicationController
+
+  def index
+
+  end
+
+end
---
+++ test/controllers/biller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BillerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
