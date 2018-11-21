3627ceeab4117ad22672ee6075af325ca07aba00
kiburei <gooddeeds17@gmil.com>
2018-11-21 23:44:20 +0300

    sdcsdc

--- app/controllers/biller_controller.rb
+++ app/controllers/biller_controller.rb
@@ -15,15 +15,21 @@ class BillerController < ApplicationController
     mpesa_number = Bill.find(params[:id]).mpesa_number
     amount = Bill.find(params[:id]).amount
     Bill.find(params[:id]).update(status: "Invoiced", user_action: "Invoiced", channel_used: "Mpesa", channel_response:  (0...8).map { (65 + rand(26)).chr }.join)
-    response = RestClient::Request.execute(
-      method: :post,
-      url: 'http://processor.nouveta.co.ke/index.php',
-      params: {PhoneNumber: "0728592629", paymentMode: 'Mpesa', AccountReference: 'Biller', Amount: 1, api_key: "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"},
-      headers: {api_key: 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'}
-    )
-    # RestClient.post 'http://processor.nouveta.co.ke/index.php', {PhoneNumber: "0720967297", paymentMode: 'Mpesa', AccountReference: 'Biller', Amount: 1, api_key: "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"}
-    redirect_to root_url
+    # response = RestClient::Request.execute(
+    #   method: :post,
+    #   url: 'http://processor.nouveta.co.ke/index.php',
+    #   params: {PhoneNumber: "0728592629", paymentMode: 'Mpesa', AccountReference: 'Biller', Amount: 1, api_key: "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"},
+    #   headers: {api_key: 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'}
+    # )
+    # a = RestClient.post 'http://processor.nouveta.co.ke/index.php', {PhoneNumber: "0728592629", paymentMode: 'Mpesa', AccountReference: 'Biller', Amount: 1, api_key: "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"}
 
+    resource = RestClient::Resource.new(
+      "http://processor.nouveta.co.ke/index.php",
+      :timeout => 1,
+      :open_timeout => 1)
+    response = resource.get :params => {PhoneNumber: "0728592629", paymentMode: 'Mpesa', AccountReference: 'Biller', Amount: 1, api_key: "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"}
+
+    redirect_to root_url
   end
 
   def callback
