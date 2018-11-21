f1c1415a2841d7eb08df1dbc33d70397f0bc52da
kiburei <gooddeeds17@gmil.com>
2018-11-21 23:09:20 +0300

    sdsdc

--- app/controllers/biller_controller.rb
+++ app/controllers/biller_controller.rb
@@ -14,8 +14,8 @@ class BillerController < ApplicationController
   def push
     mpesa_number = Bill.find(params[:id]).mpesa_number
     amount = Bill.find(params[:id]).amount
+    Bill.find(params[:id]).update(status: "Invoiced", user_action: "Invoiced", channel_used: "Mpesa" channel_response:  (0...8).map { (65 + rand(26)).chr }.join)
     RestClient.post 'http://processor.nouveta.co.ke/index.php', {PhoneNumber: "0720967297", paymentMode: 'Mpesa', AccountReference: 'Biller', Amount: 1, api_key: "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3"}
-    Bill.find(params[:id]).update(status: "STK Request", user_action: "Invoiced", channel_used: "Mpesa")
     redirect_to root_url
   end
 
